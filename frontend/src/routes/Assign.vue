fde5282914e740d1e449914b4ef03d046960adee
Michael Jonathan <michaeljonathan664@gmail.com>
2023-12-17 11:17:25 +0700

    Update Assign.vue

--- frontend/src/routes/Assign.vue
+++ frontend/src/routes/Assign.vue
@@ -16,26 +16,49 @@
             style="width: 250px"
             @search="onSearch"
             /> -->
-            <a-button v-if="!startScan" @click="startScan=true" type="primary">Scan Location</a-button>
-            <div v-else class="stream">
-              <qr-stream @decode="onDecode" class="mb">
+            <a-button v-if="!startScan" @click="openScan" type="primary">Scan Location</a-button>
+            <div id="qr-code-full-region">
+              <!-- <qr-stream @decode="onDecode" class="mb">
                 <div style="color: red;" class="frame"></div>
-              </qr-stream>
+              </qr-stream> -->
+              
+
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import axios from 'axios'
 import {apiHead} from '../helper'
 import {message} from 'ant-design-vue'
 import {QrStream} from 'vue3-qr-reader'
+import {Html5QrcodeScanner} from 'html5-qrcode'
 
 const trackingNumber = ref('')
 const lockerCode = ref('')
 const startScan = ref(false)
+const config = {
+        fps: 1,
+        qrbox: 250,
+        rememberLastUsedCamera: true,
+        aspectRatio: 4/3,
+        showTorchButtonIfSupported: true,
+    }
+var html5QrcodeScanner = null
+onMounted(() =>{
+    html5QrcodeScanner = new Html5QrcodeScanner('qr-code-full-region', config);
+})
+const openScan = () =>{
+    startScan.value=true
+    html5QrcodeScanner.render(onDecode);
+}
+
+const onScanSuccess = (decodedText, decodedResult) =>{
+    alert(decodedText)
+    console.log(decodedText)
+}
 const onDecode = (e) =>{
     
     let data = {
