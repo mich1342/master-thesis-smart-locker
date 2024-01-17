878a966a59c60e6619d186a73e673362494c60e3
mich1342 <michaeljonathan664@gmail.com>
2024-01-17 21:15:16 +0700

    add retry db connection

--- backend/helper/pgconn.py
+++ backend/helper/pgconn.py
@@ -5,21 +5,23 @@ con = cur = db = None
 
 def connect():
     global con, cur, db
-    try:
-        con = psycopg2.connect(
-            database='postgres',
-            user='postrges',
-            password='postgres',
-            host='db-thesis',
-            port='5432'
-            )
-        cur = con.cursor()
-        print("Connection Open")
-    except (psycopg2.DatabaseError):
-        if con:
-            con.rollback()
-        print(psycopg2.DatabaseError)
-        sys.exit
+    while True:
+        try:
+            con = psycopg2.connect(
+                database='postgres',
+                user='postrges',
+                password='postgres',
+                host='db-thesis',
+                port='5432'
+                )
+            cur = con.cursor()
+            print("Connection Open")
+            break
+        except (psycopg2.DatabaseError):
+            if con:
+                con.rollback()
+            print(psycopg2.DatabaseError)
+            sys.exit()
 
 
 def get_db():
