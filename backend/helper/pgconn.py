5e3ff4f76b1345316d4bad8dae3ca45c83597bd4
mich1342 <michaeljonathan664@gmail.com>
2024-05-14 10:45:57 +0700

    add init procedure for backend

--- backend/helper/pgconn.py
+++ backend/helper/pgconn.py
@@ -16,6 +16,23 @@ def connect():
                 port='5432'
                 )
             cur = con.cursor()
+            cur.execute('''
+                DROP TABLE IF EXISTS transaction;
+                CREATE TABLE transaction (
+                    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                    locker VARCHAR(20),
+                    box VARCHAR(20),
+                    package_code VARCHAR(40),
+                    qr_code VARCHAR(40),
+                    status VARCHAR(20)
+                );
+                INSERT INTO transaction (locker, box, qr_code, status)
+                VALUES
+                ('AAA', '001', 'idle', 'IDLE'),
+                ('AAA', '002', 'idle', 'IDLE'),
+                ('AAA', '003', 'idle', 'IDLE');
+                ''')
+            con.commit()
             print("Connection Open")
             break
         except (psycopg2.DatabaseError):
